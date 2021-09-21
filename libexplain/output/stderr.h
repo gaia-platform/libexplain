9eeaf79c6e985fddcbeec433eeed3f21c07de852
Simone Rondelli <simone@gaiaplatform.io>
2021-09-21 11:17:22 -0700

    Author: Andreas Beckmann <anbe@debian.org> Description: fix FTBFS with GCC 10  GCC 10 defaults to -fno-common

--- libexplain/output/stderr.h
+++ libexplain/output/stderr.h
@@ -22,7 +22,7 @@
 
 #include <libexplain/output.h>
 
-explain_output_t explain_output_static_stderr;
+extern explain_output_t explain_output_static_stderr;
 
 #endif /* LIBEXPLAIN_OUTPUT_STDERR_H */
 /* vim: set ts=8 sw=4 et : */
